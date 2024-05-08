dbbead28ad7521ee50a4f8c299178e36b54e7280
MilanJaukl <97347968+MilanJaukl@users.noreply.github.com>
2024-05-08 18:33:32 +0000

    chore: Add hasRole method to User model and update dependencies

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,10 @@
+<x-layout.default>
+    <x-slot name="title">
+        Login
+    </x-slot>
+
+    <div class="container">
+        <h1>a</h1>
+        <button class="btn btn-primary">ok</button>a
+    </div>
+</x-layout.default>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\UserController;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/login', [UserController::class, 'login'])->name('get.user.login');
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/admin', [AdminController::class, 'index'])->name('get.admin.index');
 });
